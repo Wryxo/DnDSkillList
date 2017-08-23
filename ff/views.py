7b2a9ecdc7143bc28bf6fa613d15e2875c6586e5
Wryxo <wryxsk@gmail.com>
2017-08-24 00:52:04 +0200

    uz mam dataaaa

--- ff/views.py
+++ ff/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def generateImage(request):
-    return render(request, 'ff/generateImage.html')
+    return render(request, 'ff/generateImage.html', {'data': json.dumps(request.POST)})
