b9f1650015414d57b3e1a14dc435b7aaf3640cb0
Wryxo <wryxsk@gmail.com>
2017-08-24 00:30:38 +0200

    chcem dataaa

--- ff/views.py
+++ ff/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Skill
 
 # Create your views here.
 def generateImage(request):
     if request.method == 'GET':
-        return render(request, 'ff/generateImage.html', {'hura': 'nudaaaa'})
+        skills = Skill.objects.all().order_by('name')
+        return render(request, 'ff/generateImage.html', {'hura': 'nudaaaa', 'skills': skills})
     if request.method == 'POST':
         return render(request, 'ff/generateImage.html', {'data': request.POST, 'hura': 'hura'})
