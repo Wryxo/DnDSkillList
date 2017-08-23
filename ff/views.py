9dc79801878ba2064e585840d7f480bb65c800c1
Wryxo <wryxsk@gmail.com>
2017-08-24 00:32:20 +0200

    chcem dataaaqwe

--- ff/views.py
+++ ff/views.py
@@ -5,6 +5,6 @@ from .models import Skill
 def generateImage(request):
     if request.method == 'GET':
         skills = Skill.objects.all().order_by('name')
-        return render(request, 'ff/generateImage.html', {'hura': 'nudaaaa', 'skills': skills})
+        return render(request, 'ff/generateImage.html', {'hura': request.GET, 'skills': skills})
     if request.method == 'POST':
         return render(request, 'ff/generateImage.html', {'data': request.POST, 'hura': 'hura'})
