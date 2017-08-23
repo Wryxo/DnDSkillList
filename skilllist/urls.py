3395ff58054da7168d0b0a23d96d7e33440483ac
Wryxo <wryxsk@gmail.com>
2017-08-24 01:15:12 +0200

    terji

--- skilllist/urls.py
+++ skilllist/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.generateImage, name='generateImage'),
-    url(r'^skilllist$', views.skill_list, name='skill_list'),
+    url(r'^generateImage$', views.generateImage, name='generateImage'),
+    url(r'^$', views.skill_list, name='skill_list'),
     url(r'^asheron$', views.asheron, name='asheron'),
     url(r'^marjory$', views.marjory, name='marjory'),
 ]
