475621bab1518acfd2120e71e950ea4a912efd80
Wryxo <wryxsk@gmail.com>
2017-08-24 01:09:23 +0200

    qwetrt

--- ff/views.py
+++ ff/views.py
@@ -3,4 +3,5 @@ import json
 
 # Create your views here.
 def generateImage(request):
-    return render(request, 'ff/generateImage.html', {'data': request})
+    sadf = request.POST['team1']
+    return render(request, 'ff/generateImage.html', {'data': request.POST})
