f8431b809c970d78a9b9cfc6dad22aaebc98ae36
Wryxo <wryxsk@gmail.com>
2017-08-24 00:26:12 +0200

    chcem data

--- ff/views.py
+++ ff/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def generateImage(request):
     if request.method == 'GET':
-        return render(request, 'ff/generateImage.html')
+        return render(request, 'ff/generateImage.html', {'hura': 'nudaaaa'})
     if request.method == 'POST':
-        return render(request, 'ff/generateImage.html', {'data': request.POST})
+        return render(request, 'ff/generateImage.html', {'data': request.POST, 'hura': 'hura'})
